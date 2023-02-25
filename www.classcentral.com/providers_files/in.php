8b10cd4a778a6ac0a0fb82a958e264bd545560cc
Juan <salamancajd14@gmail.com>
2023-02-25 11:46:01 -0500

    full website

---
+++ www.classcentral.com/providers_files/in.php
@@ -0,0 +1,8 @@
+// (☞ﾟ∀ﾟ)☞ static31
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '806743796');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100717247', 'no', 600 );
+_heatmaps_g2g_100717247='no';
+}
